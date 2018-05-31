ade53c916cc70340223aca92f33e0450167323b9
Ernesto Chaves Chaves <ernestochaveschaves@outlook.com>
2018-05-31 08:30:17 -0600

    Added sample website

---
+++ Lab 02 Clean Code/Reminders/Reminders/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reminders.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<Reminder> reminders = GetReminders();
+
+            return View(reminders);
+        }
+
+        private static List<Reminder> GetReminders()
+        {
+            List<Reminder> reminders = new List<Reminder>();
+            for (int i = 0; i < 30; i++)
+            {
+                reminders.Add(new Reminder(i, "Fake Reminder " + i, DateTime.Now.AddDays(i)));
+            }
+
+            return reminders;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Lab 02 Clean Code/Reminders/Reminders/Controllers/Reminder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Reminders.Controllers
+{
+    public class Reminder
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public Reminder(int id, string name, DateTime date)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Date = date;
+        }
+    }
+}
